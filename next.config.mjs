39e2677d902e5ad26329b70ef08bd5e5a8cafc26
faizan-abdul-haq <faizanabdulhaq@barqsol.com>
2025-02-10 11:36:00 +0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default withSentryConfig(
+  nextConfig,
+  {
+    silent: true,
+    org: "javascript-mastery",
+    project: "javascript-nextjs",
+  },
+  {
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+
+    // Menonaktifkan plugin Webpack untuk menghindari upload source maps
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  }
+);
